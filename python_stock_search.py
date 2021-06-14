4f8a55a696616d79b77a3fd46de0fcca40f9b123
kayn0111034 <0111034@student.isf.edu.hk>
2021-06-14 12:10:35 +0800

    return dict instead of print dict

--- python_stock_search.py
+++ python_stock_search.py
@@ -4,36 +4,40 @@ from selenium.webdriver import Chrome
 import time
 
 
-with Chrome() as driver:
-    #your code inside this indent
+def stock_scraper(stock):
+    with Chrome() as driver:
+        #your code inside this indent
 
-    #gets the tsla webpage on CNBC
-    stock=["ba","tsla"]
-    for i in range(len(stock)):
-        driver.get("https://www.cnbc.com/quotes/"+stock[i])
+        #gets the tsla webpage on CNBC
+        #stock=["ba","tsla"]
+        for i in range(len(stock)):
+            driver.get("https://www.cnbc.com/quotes/"+stock[i])
 
-        stock_name=driver.find_element_by_class_name('QuoteStrip-name').text
-        close_price=driver.find_element_by_class_name('QuoteStrip-lastPrice').text
+            stock_name=driver.find_element_by_class_name('QuoteStrip-name').text
 
-
-
-        close="Close:"+close_price
-        dict={
-        stock[i]:[stock_name,close]
-        }
-        # print(stock_name+":")
-        # print(close+":")
-        # print(close_price)
-        print(dict)
-    #assert "TSLA" in driver.title
-
-    #elements=driver.find_element_by_class_name('QuoteStrip-lastPriceStripContainer')
-
-    #return Close_price
-    #print(driver.title)
-    #print(dict)
+            #relative xpath selector because the class name was used in multiple places
+            open_price=driver.find_element_by_xpath('//*[@id="MainContentContainer"]/div/div[2]/div[1]/div[5]/div[2]/section/div[1]/ul/li[1]/span[2]').text
+            #relative xpath selector because class name was used in the whole table
+            close_price=driver.find_element_by_xpath('//*[@id="MainContentContainer"]/div/div[2]/div[1]/div[2]/div[4]/div[1]/div[2]/span[1]').text
 
 
-    time.sleep(5)
-    #quits the driver, ends browser session
-    driver.quit()
+            open="Open:"+open_price
+            close="Close:"+close_price
+
+            dict={
+            stock[i]:[stock_name,open,close]
+            }
+            # print(stock_name+":")
+            # print(close+":")
+            # print(close_price)
+            return dict
+        #assert "TSLA" in driver.title
+
+        #return Close_price
+        #print(driver.title)
+        #print(dict)
+
+
+        time.sleep(5)
+        #quits the driver, ends browser session
+        driver.quit()
