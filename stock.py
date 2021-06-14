f03c0f57d89ce3eb6e5d41895de0d2b929b6b28a
kayn0111034 <0111034@student.isf.edu.hk>
2021-06-14 12:11:05 +0800

    added a runfile for stock_search

---
+++ stock.py
@@ -0,0 +1,16 @@
+from python_stock_search import stock_scraper
+#
+# #stock=[]
+#
+stock_string=input("enter your stock:")
+#
+# print(stock_string)
+stock=stock_string.split()
+# print(stock)
+#
+# #print(stock_scraper(stock))
+# stock = "tsla ba"
+#print(stock_string)
+print(stock_scraper(stock))
+
+#print(stock)
