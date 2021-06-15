ca6c78d6fc97213c491d452615ab0f973b598e6a
kayn0111034 <0111034@student.isf.edu.hk>
2021-06-15 22:47:36 +0800

    #update: cnbc has different xpaths (at least for some stocks)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # stock-web-scraper
 A web-scraping bot for stock prices
+#update
+cnbc has different xpaths (at least for some stocks)
+i have yet again been ip banned (not sure if its temporary or not) so im going to discontinue this project for now (unfortunately)
--- stock.py
+++ stock.py
@@ -1,7 +1,8 @@
 from python_stock_search import stock_scraper
 #
 # #stock=[]
-#
+#update: cnbc has different xpaths (at least for some stocks)
+#and i have yet again been ip banned so im going to discontinue this project for now
 stock_string=input("enter your stock:")
 #
 # print(stock_string)
